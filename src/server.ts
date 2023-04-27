a670ce3a977b3cfbc5ed519ac8c55a2a7a7745fa
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-26 21:01:14 -0300

    chore: create application structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+
+const app = fastify()
+
+
+app.get('/hello', () => {
+  return 'Teste'
+})
+
+app.listen({
+  port: 3333,
+}).then(() => console.log('http server running 🚀'))
